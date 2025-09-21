409ea802bcffbca65066da34be513a4d2ceab372
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-21 10:59:22 +0300

    feat: add server validation via better auth server instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
-
+import { username } from "better-auth/plugins/username";
+import { validator } from "validation-better-auth";
+import {
+  emailLoginSchema,
+  registerSchema,
+  usernameLoginSchema,
+} from "./validations/auth";
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
@@ -9,4 +15,22 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  appName: "Kokoro",
+  plugins: [
+    username(),
+    validator([
+      {
+        path: "sign-up/email",
+        schema: registerSchema,
+      },
+      {
+        path: "sign-in/email",
+        schema: emailLoginSchema,
+      },
+      {
+        path: "sign-in/username",
+        schema: usernameLoginSchema,
+      },
+    ]),
+  ],
 });
