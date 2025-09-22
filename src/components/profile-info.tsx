2ed13a4ff42f2ec988a9fb8df38c3251f7508f16
COCO <colomberakotonjanahary@gmail.com>
2025-09-22 11:26:12 +0300

    Feat/user session (#5)
    * feat: add login/register pages and basic navigation on homepage
    
    * feat: add responsive animated circle to auth page background
    
    * feat: create responsive animated mockup auth page with toggle between login and register
    
    * fix: auth form animations and ball positioning for desktop and mobile
    
    * feat: replace external toggle with internal form switching buttons
    
    * refactor: remove seperate login and register page since its not needed anymore
    
    * feat: setup prisma model and implement betterauth and prisma entities
    
    * feat: add better auth mount handler
    
    * feat: create better auth client instance
    
    * feat: ad zod validations
    
    * feat: add server validation via better auth server instance
    
    * feat: implement signup logic that allows user to register
    
    * fix: add different schema validation for betterauth default attributes
    
    * refactor: make passwords to be at least 8 characters
    
    * feat: login logic, users can log with email
    
    * feat: login logic, users can log with username
    
    * refactor: add success toast message on username login and remove debug logs
    
    * feat: implement logout logic
    
    * feat: redirect users to the right page according to the session
    
    * feat: configure google provider in betterauth
    
    * feat: implement google sign in
    
    * feat: implement github sign in
    
    * feat: implement facebook sign in
    
    * fix: enable account linking so that when a user try to log in with a provider after signing up with the used email work
    
    * chore: unuse prisma generated client
    
    * refactor: remove tiktok social login and related config
    
    * feat: oauth buttons in register form
    
    * feat: redirect oauth errors to login page, users dont need to know what the error was, they just want to try logging in again
    
    * feat: user zustand store
    
    * feat: profile initializer component placed in root component in order to get latest user info
    
    * feat: add user info in profile to showcase it works
    
    * feat: middleware

--- src/components/profile-info.tsx
+++ src/components/profile-info.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { useUserStore } from "@/app/store/useUserStore";
 import SignOutButton from "./auth/sign-out-button";
 
 const ProfileInfo = () => {
+  const user = useUserStore((state) => state.user);
+  const isLoadingUser = useUserStore((state) => state.isLoadingUser);
+
+  if (isLoadingUser || !user) return <div>Loading...</div>;
+
   return (
     <div className="flex flex-col gap-5">
       <h1 className="font-semibold text-xl">Profile</h1>
+      <div className="flex space-y-4">
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">First name</span>
+          <span>{user.firstName}</span>
+          <span className="font-semibold">Last name</span>
+          <span>{user.lastName}</span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold">Email</span>
+          <span>{user.email}</span>
+        </div>
+      </div>
       <SignOutButton />
     </div>
   );
