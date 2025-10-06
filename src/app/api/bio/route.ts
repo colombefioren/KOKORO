0edc3b7633b5ebc2f8b45ddce3501a151df90b1d
colombefioren <colomberakotonjanahary@gmail.com>
2025-10-06 11:06:22 +0300

    chore: add openapi spec and configure swagger ts client gen

---
+++ src/app/api/bio/route.ts
@@ -0,0 +1,32 @@
+import { auth } from "@/lib/auth/auth";
+import prisma from "@/lib/db/prisma";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: Request) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  }
+
+  const newBio: string = await request.json();
+
+  try {
+    await prisma.user.update({
+      where: { id: session.user.id },
+      data: { bio: newBio },
+    });
+    return NextResponse.json(
+      { success: "Bio updated successfully" },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Failed to update bio" },
+      { status: 500 }
+    );
+  }
+};
