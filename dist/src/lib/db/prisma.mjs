924dece8e885e7d3e9830d1d1a79e67ea8a0a5bb
colombefioren <colomberakotonjanahary@gmail.com>
2025-10-14 16:04:04 +0300

    chore: remove generated dist folder and its content

--- dist/src/lib/db/prisma.mjs
+++
@@ -1,9 +0,0 @@
-"use strict";
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-const globalForPrisma = global;
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-}
-export default prisma;
