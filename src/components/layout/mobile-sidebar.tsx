468a0ad39fa8f7aaaaf6a1c9abf0abcb83858f10
colombefioren <colomberakotonjanahary@gmail.com>
2026-01-09 18:16:58 +0300

    chore: upgrade next

--- src/components/layout/mobile-sidebar.tsx
+++ src/components/layout/mobile-sidebar.tsx
@@ -83,7 +83,7 @@ const MobileSidebar = () => {
       <div className="lg:hidden fixed top-4 left-4 z-[100]">
         <Button
           onClick={() => setIsOpen(!isOpen)}
-          className="w-12 h-12 rounded-xl bg-gradient-to-r from-light-royal-blue to-plum border border-light-royal-blue/30 hover:from-light-royal-blue/30 hover:to-plum/30"
+          className="w-12 h-12 transition-all duration-300 rounded-xl bg-gradient-to-r from-light-royal-blue to-plum border border-light-royal-blue/30 hover:from-light-royal-blue/90 hover:to-plum/90"
           size="icon"
         >
           <Menu className="w-6 h-6 text-white" />
