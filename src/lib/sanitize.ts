e53d96d85ec74cef04c612e4c163f205e660a5a9
colombefioren <colomberakotonjanahary@gmail.com>
2026-09-14 14:21:20 +0300

    fix: sanitize.ts type cast

--- src/lib/sanitize.ts
+++ src/lib/sanitize.ts
@@ -52,8 +52,8 @@ export interface SafeUser {
 }
 
 export function toSafeUser(user: Record<string, unknown>): SafeUser {
-  const { email: _, ...safeUser } = user as { email?: unknown; [k: string]: unknown };
-  void _;
+  const { email, ...safeUser } = user as { email?: unknown; [k: string]: unknown };
+  void email;
   return safeUser as unknown as SafeUser;
 }
 
